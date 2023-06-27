cfa95915fe4e555bea1f5cbfb064a2ad1cce8d6b
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2023-06-27 15:43:04 -0500

    Initial commit

---
+++ src/components/ProjectTable.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import projects from '@/components/projects.json'
+import { Container } from '@/components/Container'
+
+function ProjectTable() {
+  return (
+    <section
+      id="projects"
+      aria-labelledby="projects-title"
+      className="pb-16 pt-20 sm:pb-24 sm:pt-32"
+    >
+      <Container>
+        <div className="px-4 sm:px-6 lg:px-8">
+          <div className="mt-8 flow-root">
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                      >
+                        Project
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Type
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Description
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Status
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Priority
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {projects.map((project) => (
+                      <tr key={project.Project}>
+                        <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                          <div className="flex items-center">
+                            <div className="ml-4">
+                              <div className="font-medium text-gray-900">
+                                {project.Project}
+                              </div>
+                              <div className="mt-1 text-gray-500">
+                                {project['Length (Mi)']} miles
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                          <div className="mt-1 text-gray-500">
+                            {project.Facility}
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                          <div className="text-gray-900">{`From ${project['From']} to ${project['To']} on ${project['Street']}`}</div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                          <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                            Active
+                          </span>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                          {project['Score']}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default ProjectTable
